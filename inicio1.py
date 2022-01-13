d0359d8962f5e68eeb9875e4411ec91e516b9742
Javier Pérez Martínez <2021212@iesluisbraille.es>
2022-01-13 12:12:31 +0100

    Mi primera version

---
+++ inicio1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 12:09:13 2021
+
+@author: JavierPérezMartínez
+"""
+print("¿Cómo se llama?")
+nombre = input()
+print(f"Me alegro de conocerle, {nombre}")
+
+cantidad = int(input("Digame una cantidad en pesetas:"))
+print(f"{cantidad}pesetas son {round(cantidad/166.386,2)}euros")
+
